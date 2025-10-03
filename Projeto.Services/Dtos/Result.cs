4d815b8c2b5ef49087c5185add882b0ae6500729
Pietro Roque <pietronieroboss@gmail.com>
2025-10-03 00:00:33 -0300

    feat: classe result para retorno de resultados dos serviços

---
+++ Projeto.Services/Dtos/Result.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Projeto.Services.Dtos;
+
+public class Result
+{
+    private Result(bool isSuccess, string? mensagem)
+    {
+        IsSuccess = isSuccess;
+        Message = mensagem;
+    }
+
+    public bool IsSuccess { get; init; }
+
+    [JsonPropertyName("mensagem")]
+    public string? Message { get; init; }
+
+    public static Result Succes() => new(true, null);
+    public static Result Fail(string message) => new(false, message);
+}
